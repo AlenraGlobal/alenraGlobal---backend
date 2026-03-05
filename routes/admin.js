88b6e612cd847cfbf3cf76cfe94ac7fbeeae6307
k1-karim <kawthark005@gmail.com>
2026-03-05 14:12:55 +0000

    fixed admin route

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,10 @@ const Contact = require('../models/Contact');
 
 const router = express.Router();
 
+router.options("*", (req, res) => {
+  res.sendStatus(200);
+});
+
 // Dashboard welcome
 router.get('/dashboard', adminAuth, (req, res) => {
   res.json({
